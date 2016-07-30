73765b7da62a464c7d0e3bb7a286e737b6cf72fa
erichaydel <erichaydel@gmail.com>
2016-07-30 15:03:17 -0400

    Put err name into initializer on errdo_generate

--- lib/generators/errdo/errdo_generator.rb
+++ lib/generators/errdo/errdo_generator.rb
@@ -15,6 +15,24 @@ module Errdo
         instance.invoke controller, [instance.name]
       end
 
+      def add_error_name_to_initializer
+        inject_into_file 'config/initializers/errdo.rb', before: "\nend" do
+          <<-TEXT
+
+
+    # This is the name of the class inside your application that the exceptions are stored as.
+    # Exceptions are a reserved class name.
+    #
+    # If you want to rename it, make sure you also change the migration that comes with the gem.
+    # Setting this to nil means that errors won't be tracked in the database
+    # Note: The model name "{name}_instance" should also be free
+    #
+    # Installing with the option --no-database should set this correctly
+    config.error_name = :#{file_name}
+          TEXT
+        end
+      end
+
     end
   end
 end
--- test/generators/errdo_generator_test.rb
+++ test/generators/errdo_generator_test.rb
@@ -1,6 +1,4 @@
 require "generators/errdo/errdo_generator"
-require "generators/active_record/errdo_generator"
-
 require 'test_helper'
 
 class ErrdoGeneratorTest < Rails::Generators::TestCase
@@ -8,10 +6,20 @@ class ErrdoGeneratorTest < Rails::Generators::TestCase
   tests Errdo::Generators::ErrdoGenerator
   destination File.expand_path("../../tmp", __FILE__)
   setup :prepare_destination
+  setup :make_initializer
 
-  # should "default to name 'error'" do
-  #   run_generator
-  #   assert_migration "db/migrate/errdo_create_errors.rb", /def change/
-  # end
+  should "add class name to initializer" do
+    run_generator %w(err)
+    assert_file "config/initializers/errdo.rb", /config.error_name = :err/
+  end
+
+  private
+
+  def make_initializer
+    mkdir_p "#{destination_root}/config/initializers/"
+    File.open("#{destination_root}/config/initializers/errdo.rb", 'w') do |file|
+      file.write("Errdo.setup do |config|\nend")
+    end
+  end
 
 end
