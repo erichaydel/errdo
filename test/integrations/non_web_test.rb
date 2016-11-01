632cbfa20a1a2a0af56cab77ce56b1f3518a670e
erichaydel <erichaydel@gmail.com>
2016-10-31 20:44:26 -0400

    Break up a fat test model

--- test/integrations/non_web_test.rb
+++ test/integrations/non_web_test.rb
@@ -12,6 +12,46 @@ end
 
 class NonWebTest < ActionDispatch::IntegrationTest
 
+  # rubocop:disable Style/RescueModifier
+  # rubocop:disable Lint/HandleExceptions
+  context "rake tasks" do
+    setup do
+      load "#{Rails.root}/lib/tasks/test.rake"
+    end
+
+    teardown do
+      Rake.application['test:error'].reenable
+      Rake.application['test:interrupt'].reenable
+    end
+
+    should "create an error when a task fails" do
+      Errdo.log_task_exceptions = true
+      assert_difference 'Errdo::ErrorOccurrence.count', 1 do
+        Rake.application['test:error'].invoke rescue ""
+      end
+    end
+
+    should "not create an error when a task fails with an interrupt" do
+      Errdo.log_task_exceptions = true
+      load "#{Rails.root}/lib/tasks/test.rake"
+      assert_difference 'Errdo::ErrorOccurrence.count', 0 do
+        begin
+          Rake.application['test:interrupt'].invoke
+        rescue Interrupt
+        end
+      end
+    end
+
+    should "create not an error when a task fails when not set" do
+      Errdo.log_task_exceptions = false
+      assert_difference 'Errdo::ErrorOccurrence.count', 0 do
+        Rake.application['test:error'].invoke rescue ""
+      end
+    end
+  end
+  # rubocop:enable Style/RescueModifier
+  # rubocop:enable Lint/HandleExceptions
+
   context "active jobs" do
     should "log to errdo when job fails asynchronously" do
       assert_difference 'Errdo::ErrorOccurrence.count', 1 do
