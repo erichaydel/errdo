bb2b3d09c5d1e6830432aecdd3a99a64a11a38a5
Tom Jaklitsch <thomas.m.jaklitsch@gmail.com>
2017-11-18 21:59:50 +0000

    Most of index page done

--- app/controllers/errdo/errors_controller.rb
+++ app/controllers/errdo/errors_controller.rb
@@ -8,8 +8,29 @@ module Errdo
 
     before_action :authorize_user
 
+    DEFAULT_SCOPE = "active"
+
     def index
-      @errors = Errdo::Error.order(last_occurred_at: :desc).includes(:last_experiencer).page params[:page]
+      @scope = get_whitelisted_scope
+
+      if @scope == "all"
+        base_query = Errdo::Error
+      else
+        base_query = Errdo::Error.send(@scope)
+      end
+
+      @errors = base_query.order(last_occurred_at: :desc)
+                          .includes(:last_experiencer)
+                          .page(params[:page])
+
+      @chart_data = {
+        errors:      Errdo::Error.where(created_at: 2.weeks.ago..Time.now)
+                                 .group_by_day(:created_at)
+                                 .count,
+        occurrences: Errdo::ErrorOccurrence.where(created_at: 2.weeks.ago..Time.now)
+                                           .group_by_day(:created_at)
+                                           .count
+      }
     end
 
     def show
@@ -47,5 +68,18 @@ module Errdo
       end
     end
 
+    def get_whitelisted_scope
+      if params[:scope] == "active"
+        "active"
+      elsif params[:scope] == "resolved"
+        "resolved"
+      elsif params[:scope] == "wontfix"
+        "wontfix"
+      elsif params[:scope] == "all"
+        "all"
+      else
+        DEFAULT_SCOPE
+      end
+    end
   end
 end
