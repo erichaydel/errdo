8f2b0cf5c4b2333587909c84c1a7c90e18931eff
erichaydel <erichaydel@gmail.com>
2017-11-18 16:14:10 -0500

    Fix up error show page

--- app/assets/javascripts/errdo/errdo.js
+++ app/assets/javascripts/errdo/errdo.js
@@ -1,2 +1,14 @@
+//= require highcharts
 //= require chartkick
 //= require_tree .
+
+$(document).ready(function() {
+  $('.tabs ul li').on('click', function() {
+    var div = $(this).find('a').attr('container');
+    $('.tabs ul li').removeClass('is-active');
+    $(this).addClass('is-active');
+
+    $(".tabbed").addClass('is-hidden');
+    $(div).removeClass('is-hidden');
+  });
+});
